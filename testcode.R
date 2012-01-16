d35dbad798da8e1272db64e6e28ece48dfa23451
Patrick O'Neill <synapse.and.syntax@gmail.com>
2012-01-16 03:56:10 -0500

    grad descent function

--- testcode.R
+++ testcode.R
@@ -19,13 +19,50 @@ mle <- function(xs,epsilon=.00001,delta=.00001,m1=1,m2=1,verbose=FALSE){
     m2 <- winner[2]
     old.err <- new.err
     new.err <- err(m1,m2)
-    if(verbose && gen %% 1000 == 0)
+    if(verbose && gen %% 1000 == 0){
       print(paste(m1,m2,old.err,new.err,gen))
+    }
     gen <- gen + 1
   }
   c(m1,m2)
 }
 
+grad.descent <- function(xs,epsilon=.001,delta=.00001,m1=1,m2=1){
+  c1 <- mean(log(1+exp(xs)))
+  c2 <- mean(log(1+exp(-xs)))
+  lhs1 <- function(x,y)-(digamma(y) - digamma(x + y))
+  lhs2 <- function(x,y)-(digamma(x) - digamma(x + y))
+  x <- c(m1,m2)
+  G <- function(x) matrix(c(lhs1(x[1],x[2]) - c1,lhs2(x[1],x[2]) - c2))
+  F <- function(x) t(G(x))%*%G(x)
+  Jg <- function(x) t(matrix(c(trigamma(x[1] + x[2]),
+                                -trigamma(x[2]) + trigamma(x[1] + x[2]),
+                                -trigamma(x[1]) + trigamma(x[1] + x[2]),
+                                trigamma(x[1] + x[2])),ncol=2))
+  grad.F <- function(x) t(Jg(x))%*%G(x)
+  gen <- 0
+  while(F(x) > epsilon){
+    grad <- grad.F(x)
+    hi <- delta
+    lo <- 0
+    while(hi!=lo){
+#      print(c(lo,hi))
+      d <- (hi+lo)/2
+      x.prime <- x - d * grad
+      if(F(x.prime) > F(x)){
+        hi <- d
+      }
+      else{
+        lo <- d
+      }
+    }
+    x <- x.prime
+    if(gen %% 1000 == 0)
+      print(paste(gen,x))
+  }
+  x
+    
+}
 meta.mle <- function(xs,epsilon=1,delta=1,m1=1,m2=2,factor=2){
   while(epsilon>0){
     ms <- mle(xs,epsilon,delta,m1,m2)
